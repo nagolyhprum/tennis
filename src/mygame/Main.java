483d8061ea432d8fd412bf144af0dc8ac4351118
tuc51727 <tuc51727@CIS-WCC20004.tu.temple.edu>
2013-03-27 13:39:47 -0400

    Initial Commit

---
+++ src/mygame/Main.java
@@ -0,0 +1,24 @@
+package mygame;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.renderer.RenderManager;
+
+public class Main extends SimpleApplication {
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+    }
+}
